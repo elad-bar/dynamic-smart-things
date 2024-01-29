3ddfc5a397cfe716893825de7743d8d7046ae430
Elad Bar <elad.bar@hotmail.com>
2024-01-29 15:11:37 +0200

    Fix error when attribute doesn't contain any configuration (only value is available)

--- models/attribute.py
+++ models/attribute.py
@@ -18,19 +18,20 @@ class AttributeEntity:
         _LOGGER.debug(f"Loading attribute, Data: {data}")
 
         entity = AttributeEntity()
-
-        schema = capability_attribute.get("schema", {})
-        command_name = capability_attribute.get("setter")
-
-        default_command = [
-            copy(command)
-            for command in commands
-            if command_name is not None and command.command == command_name
-        ]
-
         entity.value = data.get("value")
-        entity.properties = schema.get("properties", {}).get("value")
 
-        entity.default_command = None if len(default_command) == 0 else default_command[0]
+        if capability_attribute is not None:
+            schema = capability_attribute.get("schema", {})
+            command_name = capability_attribute.get("setter")
+
+            default_command = [
+                copy(command)
+                for command in commands
+                if command_name is not None and command.command == command_name
+            ]
+
+            entity.properties = schema.get("properties", {}).get("value")
+
+            entity.default_command = None if len(default_command) == 0 else default_command[0]
 
         return entity
